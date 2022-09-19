bf1e37d1562700f2de6d9a49480b7a0272f51b57
headbwoi <tochukwujohn24@gmail.com>
2022-09-19 10:55:07 +0100

    added fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import { useState } from "react"
+import IpInfo from "./IpInfo"
 
 function App() {
   return (
     <main className="w-full h-full relative before:absolute before:top-0 before:left-0 before:h-[18.75rem] lg:h-[17.5rem] before:w-screen before:bg-[url('./images/pattern-bg.png')]">
       <div className="container md:pt-8 px-6 md:px-10 lg:px-12 xl:px-0 relative">
         <header className="py-7 text-center">
-          <h1 className="text-xl md:text-2xl lg:text-3xl text-white">
+          <h1 className="text-xl md:text-2xl lg:text-3xl text-white font-bold">
             IP Address Tracker
           </h1>
         </header>
 
         <section className="input">
           <h2 className="sr-only">Search for any IP address or domain </h2>
-          <div className="w-full h-14 rounded-2xl relative overflow-hidden mb-6 md:mb-10 max-w-[34.6875rem] mx-auto">
+          <div className="w-full h-14 rounded-2xl relative overflow-hidden mb-6 md:mb-10 max-w-[34.6875rem] mx-auto border-none">
             <input
               type="text"
               name="ip"
-              className=" w-full h-full outline none px-5"
+              className=" w-full h-full outline none px-5 font-medium placeholder:text-darkGray text-veryDarkGray placeholder:text-sm md:placeholder:text-base"
+              placeholder="Search for any IP address or domain "
               aria-label="Search for any IP address or domain "
             />
             <button
-              className="absolute top-0 right-0 w-[3.625rem] h-full bg-veryDarkGray grid place-items-center hover:bg-veryDarkGray/[0.85] cursor-pointer"
+              className="absolute top-0 right-0 w-[3.625rem] h-full bg-veryDarkGray grid place-items-center hover:bg-veryDarkGray/[0.85] cursor-pointer border-none outline-none"
               aria-label="click to track ip provided"
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
@@ -39,6 +41,7 @@ function App() {
           <h3 className="sr-only">
             information about the IP Address you provided
           </h3>
+          <IpInfo />
         </section>
       </div>
     </main>
--- src/IpInfo.tsx
+++ src/IpInfo.tsx
@@ -2,7 +2,11 @@ import React from "react"
 
 const IpInfo = () => {
   return (
-    <div className="w-full max-w-[69.375rem] h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl "></div>
+    <div className="w-full max-w-[69.375rem] h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl bg-white shadow-xl py-6 md:py-0 flex items-center flex-col md:flex-row ">
+      <div className="text-center md:text-left block">
+        <p className="text-sm text-darkGray uppercase">IP Address</p>
+      </div>
+    </div>
   )
 }
 
