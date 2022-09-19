ceddc7b5d826bb7cde9c44f8961a3a06527fb33d
headbwoi <tochukwujohn24@gmail.com>
2022-09-19 11:00:59 +0100

    added fontsize to input, added ip address

--- src/IpInfo.tsx
+++ src/IpInfo.tsx
@@ -2,11 +2,12 @@ import React from "react"
 
 const IpInfo = () => {
   return (
-    <div className="w-full max-w-[69.375rem] h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl bg-white shadow-xl py-6 md:py-0 flex items-center flex-col md:flex-row ">
+    <div className="w-full max-w-[69.375rem] h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl bg-white shadow-xl py-6 md:py-0 md:px-7 flex items-center flex-col md:flex-row ">
       <div className="text-center md:text-left block">
-        <p className="text-sm text-darkGray uppercase font-normal">
+        <p className="text-xs md:text-sm text-darkGray uppercase font-medium">
           IP Address
         </p>
+        <p className="font-bold text-veryDarkGray text-lg">192.212.174.101</p>
       </div>
     </div>
   )
