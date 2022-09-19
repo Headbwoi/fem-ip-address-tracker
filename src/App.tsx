923cf993cd6ae7795d34e31c103ffe74a6a47ac6
headbwoi <tochukwujohn24@gmail.com>
2022-09-19 11:37:35 +0100

    fixed image load on build issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import IpInfo from "./IpInfo"
 
 function App() {
   return (
-    <main className="w-full h-full relative before:absolute before:top-0 before:left-0 before:h-[18.75rem] lg:h-[17.5rem] before:w-screen before:bg-[url('./images/pattern-bg.png')] before:bg-no-repeat before:bg-cover before:bg-center">
+    <main className="w-full h-full relative before:absolute before:top-0 before:left-0 before:h-[18.75rem] lg:h-[17.5rem] before:w-screen before:bg-[url('/images/pattern-bg.png')] before:bg-no-repeat before:bg-cover before:bg-center">
       <div className="container md:pt-8 px-6 md:px-10 lg:px-12 xl:px-0 relative">
         <header className="py-7 text-center">
           <h1 className="text-xl md:text-2xl lg:text-3xl text-white font-bold">
