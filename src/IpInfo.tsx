0e6e1ddd7aa5bbffb0f33c998dd122301db55f1c
headbwoi <tochukwujohn24@gmail.com>
2022-09-19 10:48:01 +0100

    ceated Ip info component file

---
+++ src/IpInfo.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const IpInfo = () => {
+  return (
+    <div className="w-full max-w-[69.375rem] h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl "></div>
+  )
+}
+
+export default IpInfo
