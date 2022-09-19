3065848c3f91ccfd8c960b394879d4fe4338c812
headbwoi <tochukwujohn24@gmail.com>
2022-09-19 11:07:08 +0100

    moved ip details to its own component

--- src/IpInfo.tsx
+++ src/IpInfo.tsx
@@ -1,16 +1,24 @@
-import React from "react"
-
+interface InfoDetails {
+  title: string
+  value: string
+}
 const IpInfo = () => {
   return (
     <div className="w-full max-w-[69.375rem] h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl bg-white shadow-xl py-6 md:py-0 md:px-7 flex items-center flex-col md:flex-row ">
-      <div className="text-center md:text-left block">
-        <p className="text-xs md:text-sm text-darkGray uppercase font-medium">
-          IP Address
-        </p>
-        <p className="font-bold text-veryDarkGray text-lg">192.212.174.101</p>
-      </div>
+      <IpInfoDetails title="IP Address" value="192.212.174.101" />
     </div>
   )
 }
 
 export default IpInfo
+
+const IpInfoDetails = ({ title, value }: InfoDetails) => {
+  return (
+    <div className="text-center md:text-left block">
+      <p className="text-xs md:text-sm text-darkGray uppercase font-medium md:mb-3">
+        {title}
+      </p>
+      <p className="font-bold text-veryDarkGray text-lg md:text-2xl">{value}</p>
+    </div>
+  )
+}
