2053dc34dafefc1950dc47ceb9ae1a1cfa7f132f
headbwoi <tochukwujohn24@gmail.com>
2022-09-20 16:52:57 +0100

    finally got leaflet working

--- src/App.tsx
+++ src/App.tsx
@@ -19,12 +19,11 @@ interface InfoDetails {
 
 function App() {
   const [ipAddess, setIpAddess] = useState("")
-  const [lat, setLat] = useState(0)
-  const [lng, setLng] = useState(0)
   const [city, setCity] = useState("")
   const [country, setCountry] = useState("")
   const [timezone, setTimezone] = useState("")
   const [isp, setIsp] = useState("")
+  const [map, setMap] = useState(undefined)
 
   const BaseUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${
     import.meta.env.VITE_GEO_API_KEY
@@ -35,14 +34,17 @@ function App() {
     const data = await res.json().catch((error) => {
       console.log(error)
     })
-    const { city, country, timezone, lat, lng } = data.location
+    const { city, country, timezone } = data.location
     setIpAddess(data.ip)
     setCity(city)
     setCountry(country)
     setTimezone(timezone)
-    setLat(lat)
-    setLng(lng)
     setIsp(data.isp)
+    const {
+      location: { lat, lng },
+    } = data
+    //@ts-ignore
+    setMap({ lat, lng })
   }
 
   useEffect(() => {
@@ -53,8 +55,6 @@ function App() {
     e.preventDefault()
     fetchData()
   }
-
-  console.log(lat, lng)
   return (
     <main className="w-full h-full relative before:absolute before:top-0 before:left-0 before:h-[18.75rem] lg:h-[17.5rem] before:w-screen before:bg-[url('/images/pattern-bg.png')] before:bg-no-repeat before:bg-cover before:bg-center before:z-[999]">
       <div className="container md:pt-8 px-6 md:px-10 lg:px-12 xl:px-0 relative">
@@ -110,16 +110,16 @@ function App() {
           </div>
         </section>
       </div>
-      {lat == undefined || lng == undefined ? (
+      {map == undefined ? (
         <div className="loader"></div>
       ) : (
-        <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
+        <MapContainer id="map" center={map} zoom={13} scrollWheelZoom={false}>
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[lat, lng]}>
-            <Popup>{`${city}, ${country}`}</Popup>
+          <Marker position={map}>
+            <Popup>You're here.</Popup>
           </Marker>
         </MapContainer>
       )}
