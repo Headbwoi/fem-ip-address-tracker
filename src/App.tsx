9d4d8bd93cda76ac732a890847121f676f000f19
headbwoi <tochukwujohn24@gmail.com>
2022-09-20 16:45:14 +0100

    finally got geo api working

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,60 @@
-import { FormEvent, useState } from "react"
+import { FormEvent, useEffect, useState } from "react"
 import axios from "axios"
-import IpInfo from "./IpInfo"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 
-type Data = {
-  location: {
-    city: string
-    country: string
-    lat: number | undefined
-    lng: number | undefined
-    timezone: string
-  }
-  isp: string
+// type Data = {
+//   location: {
+//     city: string
+//     country: string
+//     lat: number | undefined
+//     lng: number | undefined
+//     timezone: string
+//   }
+//   isp: string
+// }
+interface InfoDetails {
+  title: string
+  value: string
 }
 
 function App() {
-  const [ipAddess, setIpAddess] = useState<string | number>()
-  const [data, setData] = useState<Data>()
+  const [ipAddess, setIpAddess] = useState("")
+  const [lat, setLat] = useState(0)
+  const [lng, setLng] = useState(0)
+  const [city, setCity] = useState("")
+  const [country, setCountry] = useState("")
+  const [timezone, setTimezone] = useState("")
+  const [isp, setIsp] = useState("")
 
   const BaseUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${
-    import.meta.env.GEO_API_KEY
+    import.meta.env.VITE_GEO_API_KEY
   }&ipAddress=${ipAddess}`
+
+  const fetchData = async () => {
+    const res = await fetch(BaseUrl)
+    const data = await res.json().catch((error) => {
+      console.log(error)
+    })
+    const { city, country, timezone, lat, lng } = data.location
+    setIpAddess(data.ip)
+    setCity(city)
+    setCountry(country)
+    setTimezone(timezone)
+    setLat(lat)
+    setLng(lng)
+    setIsp(data.isp)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault()
-    axios
-      .get(BaseUrl)
-      .then((response) => {
-        setData(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+    fetchData()
   }
 
+  console.log(lat, lng)
   return (
     <main className="w-full h-full relative before:absolute before:top-0 before:left-0 before:h-[18.75rem] lg:h-[17.5rem] before:w-screen before:bg-[url('/images/pattern-bg.png')] before:bg-no-repeat before:bg-cover before:bg-center before:z-[999]">
       <div className="container md:pt-8 px-6 md:px-10 lg:px-12 xl:px-0 relative">
@@ -78,31 +99,45 @@ function App() {
           <h3 className="sr-only">
             information about the IP Address you provided
           </h3>
-          <IpInfo
-            ip={ipAddess}
-            timezone={data?.location.timezone}
-            isp={data?.isp}
-            city={data?.location.city}
-            country={data?.location.country}
-          />
+          <div className="w-full max-w-[69.375rem] min-h-[13.75rem] md:h-[10.3125rem] mx-auto rounded-xl bg-white shadow-xl py-6 md:py-0 md:px-7 xl:px-10 flex items-center flex-col gap-6 md:flex-row md:gap-10 lg:gap-14 ">
+            <IpInfoDetails title="IP Address" value={`${ipAddess}`} />
+            <span className=" hidden md:block  w-[1px] h-20 bg-darkGray "></span>
+            <IpInfoDetails title="Location" value={`${city}, ${country}`} />
+            <span className=" hidden md:block  w-[1px] h-20 bg-darkGray "></span>
+            <IpInfoDetails title="Timezone" value={`UTC - ${timezone}`} />
+            <span className=" hidden md:block  w-[1px] h-20 bg-darkGray "></span>
+            <IpInfoDetails title="ISP" value={`${isp}`} />
+          </div>
         </section>
       </div>
-
-      <MapContainer
-        center={[54.12, -122.078514]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        <Marker position={[37.40599, -122.078514]}>
-          <Popup></Popup>
-        </Marker>
-      </MapContainer>
+      {lat == undefined || lng == undefined ? (
+        <div className="loader"></div>
+      ) : (
+        <MapContainer center={[lat, lng]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker position={[lat, lng]}>
+            <Popup>{`${city}, ${country}`}</Popup>
+          </Marker>
+        </MapContainer>
+      )}
     </main>
   )
 }
 
 export default App
+
+const IpInfoDetails = ({ title, value }: InfoDetails) => {
+  return (
+    <div className="text-center md:text-left block md:w-1/4">
+      <p className="text-xs md:text-sm text-darkGray uppercase font-medium md:mb-3">
+        {title}
+      </p>
+      <p className="font-bold text-veryDarkGray text-lg md:text-xl lg:text-2xl">
+        {value}
+      </p>
+    </div>
+  )
+}
